a6a1f5ec57984158f980d8756c93ee2ebd2765f4
Abhinav Gupta <abhinavzweb@gmail.com>
2022-04-26 22:50:18 +0530

    initial commit

---
+++ init.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+node m.js & node r.js r1 & node r.js r2 && fg
---
+++ r.js
@@ -0,0 +1,54 @@
+import ipc from "node-ipc";
+
+const args = process.argv.slice(2);
+
+const ID = args[0];
+
+ipc.config.id = ID;
+ipc.config.retry = 1500;
+ipc.config.maxRetries = 10;
+ipc.config.silent = true;
+
+const emit = (message) => {
+  ipc.of.m.emit("app.message", {
+    id: ipc.config.id,
+    message,
+  });
+};
+
+let m, c, x, lap;
+
+ipc.connectToNet("m", function () {
+  ipc.of.m.on("connect", function () {
+    console.log(`## ${ID} connected to m ##`, ipc.config.delay);
+    emit({ name: "connected" });
+  });
+
+  ipc.of.m.on("app.message", (data) => {
+    console.log(ID, data.message);
+    switch (data.message.name) {
+      case "LAP":
+        m = ID === "r1" ? data.message.m1 : data.message.m2;
+        c = ID === "r1" ? data.message.c1 : data.message.c2;
+        x = 0;
+        clearInterval(lap);
+        lap = setInterval(() => {
+          ++x;
+          emit({ name: "POS", x, y: m * x + c });
+        }, 50);
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  ipc.of.m.on("disconnect", function () {
+    console.log(`${ID}: disconnected from m`);
+  });
+  ipc.of.m.on("kill.connection", function (data) {
+    console.log(`${ID}: m requested kill.connection`);
+    clearInterval(lap);
+    ipc.disconnect("m");
+  });
+});
